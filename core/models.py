7aa0cea3d7346616ce3ea5f3ea94dbff64cb6108
Lucashcr <lucash.rocha@hotmail.com>
2022-07-18 16:37:45 -0300

    Adiciona assunto do livro na database

--- core/models.py
+++ core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=64)
     url = models.CharField(max_length=256)
-    is_page = models.BooleanField(default=True)
+    subject = models.CharField(max_length=64)
+    is_page = models.BooleanField()
     
