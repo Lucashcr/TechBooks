f3554d52955d8eafe3ac2d306690570decf17de5
Lucashcr <lucash.rocha@hotmail.com>
2022-08-02 22:34:27 -0300

    Adiciona model Subject

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+    
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=64)
+    image = models.URLField(max_length=64)
+
+
 class Book(models.Model):
     name = models.CharField(max_length=64)
     url = models.CharField(max_length=256)
-    subject = models.CharField(max_length=64)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     is_page = models.BooleanField()
-    
