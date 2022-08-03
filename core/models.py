e1207f5a4270db8a4c72297278d748bee35fce52
Lucashcr <lucash.rocha@hotmail.com>
2022-08-03 00:19:32 -0300

    Configura campo de slug como único

--- core/models.py
+++ core/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     name = models.CharField(max_length=64)
-    slug = models.CharField(max_length=64)
+    slug = models.CharField(max_length=64, unique=True)
     image = models.URLField(max_length=64)
 
 
 class Book(models.Model):
     name = models.CharField(max_length=64)
-    slug = models.CharField(max_length=64)
+    slug = models.CharField(max_length=64, unique=True)
     url = models.CharField(max_length=256)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     is_page = models.BooleanField()
